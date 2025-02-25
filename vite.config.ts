f1817ce7eb994d8c9d66dd293759195737c001de
pedroynk <pedrolucas.rt@gmail.com>
2025-02-25 17:38:21 -0300

    feat: adiciona vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/", // Define a base correta para produção
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }
-})
+  },
+  build: {
+    outDir: "dist", // Define o diretório de saída padrão
+  },
+});
