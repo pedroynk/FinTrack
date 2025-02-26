40af382da0ee475428f20680d6606755fb997d9d
pedroynk <pedrolucas.rt@gmail.com>
2025-02-26 11:28:23 -0300

    fix: corrige problema de rotas 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  // Define o caminho base para produção
-  base: "/",
-
-  // Plugins utilizados
   plugins: [react()],
-
-  // Configurações de resolução de caminhos
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Usa "@" como atalho para o diretório src
+      "@": path.resolve(__dirname, "./src"),
     },
-  },
-
-  // Configurações de build
-  build: {
-    outDir: "dist", // Diretório de saída
-    emptyOutDir: true, // Limpa o diretório antes do build
-  },
-
-  // Configuração do servidor de desenvolvimento
-  server: {
-    port: 3000, // Define a porta local
-    open: true, // Abre o navegador automaticamente
-  },
-
-  // Configurações de preview
-  preview: {
-    port: 5000, // Porta para o preview do build
   }
-});
+})
