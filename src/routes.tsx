b6d85e839ee011bc36206c9a451a2d38ebcaca90
pedroynk <pedrolucas.rt@gmail.com>
2025-12-30 17:50:18 -0300

    hotfix: ajusta redirecionamento login

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import ProtectedRoute from "./ProtectedRoute";
 import AdminLayout from "./layouts/AdminLayout";
-import LoadingFallback from "./components/LoadingFallback"; // Import the component
+import LoadingFallback from "./components/LoadingFallback";
 
 const Login = lazy(() => import("./pages/admin/Login"));
 const Movies = lazy(() => import("./pages/admin/movies/Movies"));
@@ -12,94 +12,45 @@ const Dashboard = lazy(() => import("./pages/admin/home/Dashboard"));
 const Transactions = lazy(() => import("./pages/admin/finance/Transactions"));
 const Recurring = lazy(() => import("./pages/admin/finance/Recurring"));
 const Dimensions = lazy(() => import("./pages/admin/finance/Dimensions"));
-//const StudyDimensions = lazy(() => import("./pages/admin/study/Dimensions"));
-//const StudyQuestion = lazy(() => import("./pages/admin/study/Questions"));
-//const StudyTimer = lazy(() => import("./pages/admin/study/Timer"));
 
-
+const withSuspense = (Component: React.ReactNode) => (
+  <Suspense fallback={<LoadingFallback />}>{Component}</Suspense>
+);
 
 const router = createBrowserRouter([
-
   {
-    path: "/admin",
+    path: "/",
     element: <ProtectedRoute />,
     children: [
       {
-        path: "",
         element: <AdminLayout />,
         children: [
-          {
-            index: true,
-            element: (
-              <Suspense fallback={<LoadingFallback />}>
-                <Dashboard />
-              </Suspense>
-            ),
-          },
+          { index: true, element: withSuspense(<Dashboard />) },
+
           {
             path: "finance",
             children: [
-              {
-                path: "dashboard",
-                element: (
-                  <Suspense fallback={<LoadingFallback />}>
-                    <Dashboard />
-                  </Suspense>
-                ),
-              },
-              {
-                path: "recurring",
-                element: (
-                  <Suspense fallback={<LoadingFallback />}>
-                    <Recurring />
-                  </Suspense>
-                ),
-              },
-              {
-                path: "transactions",
-                element: (
-                  <Suspense fallback={<LoadingFallback />}>
-                    <Transactions />
-                  </Suspense>
-                ),
-              },
-              {
-                path: "dimensions",
-                element: (
-                  <Suspense fallback={<LoadingFallback />}>
-                    <Dimensions />
-                  </Suspense>
-                ),
-              },
+              { path: "dashboard", element: withSuspense(<Dashboard />) },
+              { path: "recurring", element: withSuspense(<Recurring />) },
+              { path: "transactions", element: withSuspense(<Transactions />) },
+              { path: "dimensions", element: withSuspense(<Dimensions />) },
             ],
           },
-          {
-            path: "movies",
-            element: (
-              <Suspense fallback={<LoadingFallback />}>
-                <Movies />
-              </Suspense>
-            ),
-          },
+
+          { path: "movies", element: withSuspense(<Movies />) },
         ],
       },
     ],
   },
+
   {
-    path: "login",
-    element: (
-      <Suspense fallback={<LoadingFallback />}>
-        <Login />
-      </Suspense>
-    ),
+    path: "/login",
+    element: withSuspense(<Login />),
   },
+
   {
     path: "*",
-    element: (
-      <Suspense fallback={<LoadingFallback />}>
-        <NotFound />
-      </Suspense>
-    ),
+    element: withSuspense(<NotFound />),
   },
 ]);
 
